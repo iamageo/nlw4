43da735aad0d373b9f113dd47864aeb4e440cb06
iamageo <geovani.af4@gmail.com>
2021-02-22 11:58:34 -0300

    criando método post

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,25 @@ import express from 'express'
 
 const app = express()
 
+/*
+ * GET -> BUSCA
+ * POST -> SALVAR
+ * PUT -> ALTERAR
+ * DELETE -> DELETAR
+ * PATCH -> ALTERAÇÃO ESPECÍFICA
+ */
+
+ app.get("/", (request, response)=> {
+     return response.json({ message: "olá mundo!" })
+ })
+
+ app.post("/", (request, response) => {
+    return response.json({ message: "os dados foram salvos com sucesso!" })
+
+ })
+
 app.listen(3333, () => {
-    console.log("Servidor rodando")
+    console.log("-------------------------------")
+    console.log("Servidor rodando na porta: 3333")
+    console.log("-------------------------------")
 })
