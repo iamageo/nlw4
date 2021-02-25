d15ecaaee4268a661b8ee725191f38ec76ff1da2
iamageo <geovani.af4@gmail.com>
2021-02-25 11:43:36 -0300

    criando controller, repositories e model para pesquisa

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,18 @@
 import {Router} from   'express'
 import {UserController} from './controllers/UserController'
+import {SurveysController} from './controllers/SurveysController'
 
 const router = Router();
 
+const surveysController = new SurveysController();
 const userController = new UserController();
 
 router.post("/users", userController.create);
 
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
+
+
+//24 min
 
 export {router};
